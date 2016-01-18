ad7915d8a1d6968565e3de31f91dacddd28467e7
ryonext <ryonext.s@gmail.com>
2016-01-18 22:25:15 +0900

    Save tags from REPL

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :twitter_id)
+      params.require(:user).permit(:name, :twitter_id, :tag_list)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  acts_as_taggable
+  acts_as_taggable_on :fav_games
 end
